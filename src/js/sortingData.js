f67b9d5eb5bc8250af650ed259c2f0fa082ff3a0
platon <chizzza@gmail.com>
2019-02-19 20:35:51 +0200

    suicide

--- src/js/sortingData.js
+++ src/js/sortingData.js
@@ -1,17 +1,30 @@
 ;'use strict';
 import getDataFromServer from './getDataFromServer';
 
-let table = document.querySelector('.js-table');
-let tableBody = document.querySelector('.js-table-body');
+const table = document.querySelector('.js-table');
+const tableBody = document.querySelector('.js-table-body');
 const dateButton = document.querySelector('.js-reg-date-sort');
+const idButton = document.querySelector('.js-id-sort');
 
 getDataFromServer('https://tanuhaua.github.io/datas-file-json/visitors.json ', fillTheTable(tableBody));
 dateButton.addEventListener('click', () => {
-   dateSorting(tableBody);
+   dateSorting(tableBody, 1);
+});
+idButton.addEventListener('click', () => {
+    idSorting(tableBody, 0);
 });
 
 function fillTheTable(container) {
     return function(data) {
+        data.sort((a, b) => {
+           let dateA = new Date(a['createdAt']);
+           let dateB = new Date(b['createdAt']);
+           if (dateA > dateB) {
+               return 1;
+           } else if (dateA < dateB) {
+               return -1;
+           }
+        });
         data.forEach((item) => {
             let row = document.createElement('tr');
             for (let key in item) {
@@ -25,30 +38,31 @@ function fillTheTable(container) {
     }
 }
 
-function dateSorting(bodyOftable) {
-    let cellWithData = [].map.call(bodyOftable.rows, row => {
-       return [].filter.call(row.cells, (cell, i) => {
-          if (i === 1) return cell;
-       });
-    });
+function dateSorting(bodyOfTable, n) {
+    let cellWithData = getNecessaryTd(bodyOfTable, n);
+    cellWithData.reverse();
+    repaint(bodyOfTable, cellWithData);
+}
 
+function idSorting(bodyTable, n) {
+    let cellWithData = getNecessaryTd(bodyTable, n);
     cellWithData.sort((a, b) => {
-        let dateA = a[0].innerHTML;
-        let dateB = b[0].innerHTML;
-        dateA = new Date(+dateA.slice(6), +dateA.slice(3, 5), +dateA.slice(0, 2));
-        dateB = new Date(+dateB.slice(6), +dateB.slice(3, 5), +dateB.slice(0, 2));
-        if (dateA > dateB) {
-            return 1;
-        } else if (dateA < dateB) {
-            return -1;
-        }
+        return b[0].innerHTML - a[0].innerHTML;
     });
-    repaint(bodyOftable, cellWithData);
+    repaint(bodyTable, cellWithData);
 }
 
 function repaint(tablesBody, arr) {
     tablesBody.innerHTML = '';
     arr.forEach(item => {
        tablesBody.appendChild(item[0].parentElement);
+    });
+}
+
+function getNecessaryTd(boxOfTable, n) {
+    return [].map.call(boxOfTable.rows, row => {
+        return [].filter.call(row.cells, (cell, i) => {
+            if (i === n) return cell;
+        });
     });
 }
